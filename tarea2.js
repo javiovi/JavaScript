575da2750728fe694c94d8f3fd5829e04ff29b92
javiovi <106032666+javiovi@users.noreply.github.com>
2022-08-10 20:44:47 -0300

    Add files via upload

---
+++ tarea2.js
@@ -0,0 +1,11 @@
+const Mipersona = {
+    nombre : "Javier",
+    edad : 35,
+    EresDesarrollador: false,
+    FechaNacimiento : new Date("april 28 1987"),
+    LibroFavorito : {
+        titulo:  "IT",
+        autor : "Stephen King",
+        fecha : new Date("march 1970"),
+    }
+}
