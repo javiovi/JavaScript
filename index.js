fd22a378df68ec8a04785b055b0393598eb7dec7
JaviOvi <106032666+javiovi@users.noreply.github.com>
2022-09-08 17:59:15 -0300

    Ejercicio-Errores-9

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Este es mi primer proyecto en node!")
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.json(),
+ // defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+function sError() {
+    throw new Error("sError function")
+}
+
+try {
+    sError();
+} catch (e) {
+    logger.log("error", e.toString());
+}
