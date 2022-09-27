b860b40b64d1e8fecc29a95e055e8bce14c8812d
JaviOvi <106032666+javiovi@users.noreply.github.com>
2022-09-27 18:08:02 -0300

    Add files via upload

---
+++ 12-introdepuracion/index.js
@@ -0,0 +1,14 @@
+const persona = {
+    nombre: "javier",
+    edad: 35
+
+}
+console.log(persona)
+
+function obtenDobleEdad(edad) {
+    return 2 * edad
+}
+
+const dobleEdad = obtenDobleEdad(persona.edad)
+
+console.log(dobleEdad)
