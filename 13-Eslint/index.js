65513d04af075e839af380ba6070cdbe6333e066
JaviOvi <106032666+javiovi@users.noreply.github.com>
2022-10-01 16:13:14 -0300

    Add files via upload

---
+++ 13-Eslint/index.js
@@ -0,0 +1,17 @@
+const nombre = "Javier"
+const apellido = 'Oviedo'
+const estudiante = nombre.concat(` `).concat(apellido)
+
+const estudianteMayus = estudiante.toUpperCase()
+const estudianteMinus = estudiante.toLowerCase()
+
+const estudianteLength = estudiante.length
+
+
+const inicialNombre = nombre.substring(0, 1)
+
+const finalApellido = apellido.substring(apellido.length - 1, apellido.length)
+
+const estudianteSinEspacios = estudiante.replace(/ /g, '')
+
+const nombreEnEstudiante = estudiante.includes(nombre)
